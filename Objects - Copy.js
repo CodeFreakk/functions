26ec461f4a79ccbe8a8b43449265ba1a732e0c0f
codeFreak!! <62851769+CodeFreakk@users.noreply.github.com>
2020-07-21 23:11:04 +0100

    Add files via upload

---
+++ Objects - Copy.js
@@ -0,0 +1,6 @@
+const name = 'Iron Man'
+const realName = "Tony stark"
+const SuperPower = 'Tech Enthusiast'
+const Friends = ['Widow', 'Captain','Thor']
+const ironman = {name , realName, SuperPower,Friends}
+console.log(ironman.Friends,name)
