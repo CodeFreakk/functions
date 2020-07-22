8fb907897657dcba8b5eec36a6e1e6455ef55bbc
codeFreak!! <62851769+CodeFreakk@users.noreply.github.com>
2020-07-22 23:11:15 +0100

    Add files via upload

---
+++ querySelector - Copy.js
@@ -0,0 +1,12 @@
+const ul = document.querySelector('ul#roster');
+console.log(ul)
+const li = document.querySelector('li')
+console.log(li) 
+const hello = document.getElementsByClassName('Hi')
+console.log(hello)
+
+
+const wonderWoman = document.querySelector('ul#roster')
+console.log(wonderWoman.previousSibling)
+const textNode = wonderWoman
+console.log(textNode.nodeValue)
