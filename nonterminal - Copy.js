e01445c89950f22ebcf7850d74aa7b62c9360894
codeFreak!! <62851769+CodeFreakk@users.noreply.github.com>
2020-07-23 23:40:59 +0100

    Add files via upload

---
+++ nonterminal - Copy.js
@@ -0,0 +1,12 @@
+console.log("Enter ur Input")
+process.stdin.once("data", function(data){
+
+    let input = num(data);
+
+if(date == 2){
+    document.write("correct");
+}
+else{
+    document.write("input 2");
+}
+});
