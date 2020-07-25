3d7c58c8fefd07a2eab0d1527fdef4e6212bd857
codeFreak!! <62851769+CodeFreakk@users.noreply.github.com>
2020-07-25 23:51:31 +0100

    Add files via upload

---
+++ promptemp - Copy.js
@@ -0,0 +1,25 @@
+const question = "What is the Author's real name?"
+const answer = prompt(question);
+const ans = "Samuel";
+const an = "samuel"
+const a = "SAMUEL"
+if(answer == ans){
+    document.write(alert("True"))
+
+}
+else if(answer == an){
+    document.write(alert("True"))
+
+}
+else if(answer == a){
+    document.write(alert("True"))
+
+}
+else{
+    document.write(alert("False"))
+}
+//
+//document.write(alert(`You answered ${answer}`))
+
+
+
